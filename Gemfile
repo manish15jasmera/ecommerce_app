ba7cfe2ed3dfd7c92994808114a369051d47d712
manish <manish15jasmera@gmail.com>
2024-12-23 10:24:48 +0530

    added bootstrap styling to devise and other views

--- Gemfile
+++ Gemfile
@@ -70,3 +70,5 @@ group :test do
 end
 
 gem 'devise' 
+
+gem 'devise-bootstrap-views'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def bootstrap_devise_error_messages!
+    return '' if resource.errors.empty?
+
+    html = '<div class="alert alert-danger"><ul>'
+    resource.errors.full_messages.each do |message|
+      html += "<li>#{message}</li>"
+    end
+    html += '</ul></div>'
+    html.html_safe
+  end
 end

10f3fb3ed7979959b9d5f52d3c9e5b3b12910d0f
manish <manish15jasmera@gmail.com>
2024-12-24 09:20:19 +0530

    added create cart action in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
   enum role: { customer: 'customer', admin: 'admin' }
-
+  has_one :cart, dependent: :destroy
   has_many :shipping_addresses, dependent: :destroy
 
   after_initialize do
     self.role ||= 'customer' if new_record?
   end
+  
+  after_create :create_cart
 
   validates :role, presence: true, inclusion: { in: roles.keys }
 
@@ -13,4 +15,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  private
+
+    def create_cart
+      Cart.create(user: self)
+    end
 end
